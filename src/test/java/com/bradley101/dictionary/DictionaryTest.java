fb2cbf255ed625e9c289d96bccae14fe6595a722
Shantanu Banerjee <shantanu.banerjee.vt@gmail.com>
2019-10-11 06:52:04 +0000

    updated tests as per gradle req

--- src/test/java/com/bradley101/dictionary/DictionaryTest.java
+++ src/test/java/com/bradley101/dictionary/DictionaryTest.java
@@ -4,26 +4,26 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class DictionaryTest {
-	
+
 	@Test
 	public void of() {
 		Dictionary dict = new Dictionary();
 		dict.of("test");
 		assert true;
 	}
-	
+
 	@Test
 	public void val() {
 		Dictionary dict = new Dictionary();
-		of();
-		setTo();
+		dict.of("test");
+		dict.of("test").setTo("okay working");
 		assertEquals("okay working", dict.of("test").val());
 	}
-	
+
 	@Test
 	public void setTo() {
 		Dictionary dict = new Dictionary();
-		of();
+		dict.of("test");
 		dict.of("test").setTo("okay working");
 		assert true;
 	}
